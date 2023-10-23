8067f4d0953a57ba1b9c2752a4e832a0ff444c46
Qthulhu42 <34537274+Qthulhu42@users.noreply.github.com>
2023-10-23 14:08:08 +1100

    Changed co2.py to use pytorch

--- ch5/co2.py
+++ ch5/co2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import pandas as pd
-import jax
-import jax.numpy as jnp
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -12,18 +11,18 @@ def read_data():
     return df
 
 def kernel(x,y,params):
-    k1 = params[0]**2*jnp.exp(-(x-y)**2/(2*params[1]**2))
-    k2 = params[2]**2*jnp.exp(-(x-y)**2/(2*params[3]**2)-2*jnp.sin(jnp.pi*(x-y))**2/(params[4]**2))
+    k1 = params[0]**2*torch.exp(-(x-y)**2/(2*params[1]**2))
+    k2 = params[2]**2*torch.exp(-(x-y)**2/(2*params[3]**2)-2*torch.sin(np.pi*(x-y))**2/(params[4]**2))
     k3 = params[5]**2*(1+(x-y)**2/(2*params[7]*params[6]**2))**(-params[7])
-    k4 = params[8]**2*jnp.exp(-(x-y)**2/(2*params[9]**2))+params[10]**2*(x==y)
+    k4 = params[8]**2*torch.exp(-(x-y)**2/(2*params[9]**2))+params[10]**2*(x==y)
     return k1+k2+k3+k4
 
 def gram_matrix(xs,params):
-    return jnp.array([[kernel(x1,x1,params) for x1 in xs] for x2 in xs])
+    return torch.tensor([[kernel(x1,x1,params) for x1 in xs] for x2 in xs])
 
 def surprisal(xs,ys,params):
     cov = gram_matrix(xs,params)
-    return 0.5 * (ys.T @ jnp.linalg.solve(cov,ys) + jnp.log(jnp.linalg.det(cov)))
+    return 0.5 * (ys.T @ torch.linalg.torch.solve(ys.view(-1,1),cov) + torch.log(torch.linalg.det(cov)))
 
 def plot(df):
     train = df.year_month<2004
@@ -41,19 +40,28 @@ if __name__=="__main__":
     x_train, y_train = df.year_month[train].values, df.average[train].values
     x_test,  y_test  = df.year_month[test].values,  df.average[test].values
 
+    params = torch.tensor(np.random.normal(size=11),requires_grad=True)
+    optimizer = torch.optim.Adam([params])
+    print("Optimising")
+    while True:
+        optimizer.zero_grad()
+        loss = surprisal(torch.tensor(x_train),torch.tensor(y_train),params)
+        print(loss)
+        loss.backward()
+        optimizer.step()
+    """
     params = np.random.normal(size=(5,11))*100
     print("Training")
     while True:
-        """
-        gradients = np.zeros(params.shape)
-        for x1,y1 in tqdm([*zip(x_train,y_train)]):
-            for x2,y2 in zip(x_train,y_train):
-                gradient_function = jax.jit(jax.grad(lambda p : surprisal(np.array([x1,x2]),np.array([y1,y2]),p)))
-                gradients += np.array(list(map(gradient_function,params)))
-        """
+        #gradients = np.zeros(params.shape)
+        #for x1,y1 in tqdm([*zip(x_train,y_train)]):
+        #    for x2,y2 in zip(x_train,y_train):
+        #        gradient_function = jax.jit(jax.grad(lambda p : surprisal(np.array([x1,x2]),np.array([y1,y2]),p)))
+        #        gradients += np.array(list(map(gradient_function,params)))
         gradient_function = jax.jit(jax.grad(lambda p : surprisal(x_train,y_train,p)))
         print("Computing gradients")
         gradients = np.array(list(map(gradient_function,params)))
         print(*map(lambda p : surprisal(x_train,y_train,p),params))
         params -= gradients
         print(params)
+    """
